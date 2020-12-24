1fb29a62ed7e73a7849663c2e017d0a8087c2a56
Helmoz <helmo.designer1@gmail.com>
2020-12-24 10:20:49 +0300

    Upload from gitlab

---
+++ src/Part2.hs
@@ -0,0 +1,151 @@
+module Part2 where
+
+import Part2.Types
+import Data.Foldable
+import Data.Maybe
+
+------------------------------------------------------------
+-- PROBLEM #6
+--
+-- Написать функцию, которая преобразует значение типа
+-- ColorLetter в символ, равный первой букве значения
+prob6 :: ColorLetter -> Char
+prob6 l = case l of
+  RED   -> 'R'
+  GREEN -> 'G'
+  BLUE  -> 'B'
+
+------------------------------------------------------------
+-- PROBLEM #7
+--
+-- Написать функцию, которая проверяет, что значения
+-- находятся в диапазоне от 0 до 255 (границы входят)
+prob7 :: ColorPart -> Bool
+prob7 p = (getInt p) >= 0 && (getInt p) <= 255
+
+getInt :: ColorPart -> Int
+getInt p = case p of
+  Red   int -> int
+  Green int -> int
+  Blue  int -> int
+
+------------------------------------------------------------
+-- PROBLEM #8
+--
+-- Написать функцию, которая добавляет в соответствующее
+-- поле значения Color значение из ColorPart
+prob8 :: Color -> ColorPart -> Color
+prob8 c p = case p of
+    Red   v -> Color { red = (red c) + v, green = (green c),      blue = (blue c)}
+    Green v -> Color { red = (red c),     green = (green c) + v,  blue = (blue c)}
+    Blue  v -> Color { red = (red c),     green = (green c),      blue = (blue c) + v}
+
+------------------------------------------------------------
+-- PROBLEM #9
+--
+-- Написать функцию, которая возвращает значение из
+-- ColorPart
+prob9 :: ColorPart -> Int
+prob9 p = getInt p
+
+------------------------------------------------------------
+-- PROBLEM #10
+--
+-- Написать функцию, которая возвращает компонент Color, у
+-- которого наибольшее значение (если такой единственный)
+prob10 :: Color -> Maybe ColorPart
+prob10 c = do
+  let maxVal = getMaxVal c
+  let lengthOfMaxVals = length (filter (\v -> v == maxVal) (getVals c))
+  if lengthOfMaxVals > 1 then Nothing
+    else getColorPartByValue c maxVal
+
+getMaxVal :: Color -> Int
+getMaxVal c = maximum (getVals c)
+
+getVals :: Color -> [Int]
+getVals c = [(red c), (green c), (blue c)]
+
+getColorPartByValue :: Color -> Int -> Maybe ColorPart
+getColorPartByValue c val
+  | red   c == val = Just (Red  (red c))
+  | green c == val = Just (Green (green c))
+  | blue  c == val = Just (Blue (blue c))
+------------------------------------------------------------
+-- PROBLEM #11
+--
+-- Найти сумму элементов дерева
+prob11 :: Num a => Tree a -> a
+prob11 tree = (getTreeSum (left tree)) + (root tree) + (getTreeSum (right tree))
+
+getTreeSum :: Num a => Maybe (Tree a) -> a
+getTreeSum maybeTree = case maybeTree of
+  Nothing -> 0
+  Just val -> prob11 val
+
+------------------------------------------------------------
+-- PROBLEM #12
+--
+-- Проверить, что дерево является деревом поиска
+-- (в дереве поиска для каждого узла выполняется, что все
+-- элементы левого поддерева узла меньше элемента в узле,
+-- а все элементы правого поддерева -- не меньше элемента
+-- в узле)
+prob12 :: Ord a => Tree a -> Bool
+prob12 tree = all (< (root tree)) (getLeftValsList (left tree))
+          && all (>= (root tree)) (getRightValsList (right tree))
+
+getLeftValsList maybeTree = case maybeTree of
+  Nothing -> []
+  Just val -> [(root val)] ++ getLeftValsList (left val)
+
+getRightValsList maybeTree = case maybeTree of
+  Nothing -> []
+  Just val -> [(root val)] ++ getRightValsList (left val)
+
+------------------------------------------------------------
+-- PROBLEM #13
+--
+-- На вход подаётся значение и дерево поиска. Вернуть то
+-- поддерево, в корне которого находится значение, если оно
+-- есть в дереве поиска; если его нет - вернуть Nothing
+prob13 :: Ord a => a -> Tree a -> Maybe (Tree a)
+prob13 num tree
+  | num == (root tree) = Just tree
+  | otherwise = do
+      maybeLeftTree <- left tree
+      maybeRightTree <- right tree
+      asum [(prob13 num maybeLeftTree), (prob13 num maybeRightTree)]
+
+------------------------------------------------------------
+-- PROBLEM #14
+--
+-- Заменить () на числа в порядке обхода "правый, левый,
+-- корень", начиная с 1
+prob14 :: Tree () -> Tree Int
+prob14 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #15
+--
+-- Выполнить вращение дерева влево относительно корня
+-- (https://en.wikipedia.org/wiki/Tree_rotation)
+prob15 :: Tree a -> Tree a
+prob15 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #16
+--
+-- Выполнить вращение дерева вправо относительно корня
+-- (https://en.wikipedia.org/wiki/Tree_rotation)
+prob16 :: Tree a -> Tree a
+prob16 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #17
+--
+-- Сбалансировать дерево поиска так, чтобы для любого узла
+-- разница высот поддеревьев не превосходила по модулю 1
+-- (например, преобразовать в полное бинарное дерево)
+prob17 :: Tree a -> Tree a
+prob17 = error "Implement me!"
---
+++ src/Part3.hs
@@ -0,0 +1,185 @@
+module Part3 where
+
+------------------------------------------------------------
+-- PROBLEM #18
+--
+-- Проверить, является ли число N простым (1 <= N <= 10^9)
+prob18 :: Integer -> Bool
+prob18 n = getSimpleDivisors 2 n == [n]
+
+getSimpleDivisors :: Integer -> Integer -> [Integer]
+getSimpleDivisors _ 1 = []
+getSimpleDivisors divisor n
+  | divisor * divisor > n = [n]
+  | n `mod` divisor == 0 = [divisor] ++ getSimpleDivisors divisor (n `div` divisor)
+  | otherwise = getSimpleDivisors (divisor + 1) n
+
+------------------------------------------------------------
+-- PROBLEM #19
+--
+-- Вернуть список всех простых делителей и их степеней в
+-- разложении числа N (1 <= N <= 10^9). Простые делители
+-- должны быть расположены по возрастанию
+prob19 :: Integer -> [(Integer, Int)]
+prob19 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #20
+--
+-- Проверить, является ли число N совершенным (1<=N<=10^10)
+-- Совершенное число равно сумме своих делителей (меньших
+-- самого числа)
+prob20 :: Integer -> Bool
+prob20 n = n == sum (getDivisors n)
+
+-- Делители числа без самого числа
+getDivisors :: Integer -> [Integer]
+getDivisors n = removeItem n (divisors n)
+
+-- Удалить само число из делителей
+removeItem :: Integer -> [Integer] -> [Integer]
+removeItem _ [] = []
+removeItem x (y : ys)
+  | x == y = removeItem x ys
+  | otherwise = y : removeItem x ys
+
+divisors :: Integer -> [Integer]
+divisors 1 = [1]
+divisors k =
+  k :
+  concatMap
+    (\x -> [x] ++ if k `div` x == x then [] else [k `div` x])
+    ( filter (\x -> k `mod` x == 0) $ takeWhile (\x -> x * x <= k) [2 ..])
+    ++ [1]
+
+------------------------------------------------------------
+-- PROBLEM #21
+--
+-- Вернуть список всех делителей числа N (1<=N<=10^10) в
+-- порядке возрастания
+prob21 :: Integer -> [Integer]
+prob21 n = quicksort (divisors n)
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p : xs) = (quicksort lesser) ++ [p] ++ (quicksort greater)
+  where
+    lesser = filter (< p) xs
+    greater = filter (>= p) xs
+
+------------------------------------------------------------
+-- PROBLEM #22
+--
+-- Подсчитать произведение количеств букв i в словах из
+-- заданной строки (списка символов)
+prob22 :: String -> Integer
+prob22 str = product $ (map iCount) (words str)
+
+iCount xs = toInteger (length (filter (== 'i') xs))
+
+------------------------------------------------------------
+-- PROBLEM #23
+--
+-- На вход подаётся строка вида "N-M: W", где N и M - целые
+-- числа, а W - строка. Вернуть символы с N-го по M-й из W,
+-- если и N и M не больше длины строки. Гарантируется, что
+-- M > 0 и N > 0. Если M > N, то вернуть символы из W в
+-- обратном порядке. Нумерация символов с единицы.
+prob23 :: String -> Maybe String
+prob23 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #24
+--
+-- Проверить, что число N - треугольное, т.е. его можно
+-- представить как сумму чисел от 1 до какого-то K
+-- (1 <= N <= 10^10)
+prob24 :: Integer -> Bool
+prob24 n = check 1 0
+  where
+    check :: Integer -> Integer -> Bool
+    check current sum
+        | sum == n = True
+        | sum > n = False
+        | otherwise = check (succ current) (sum + current)
+
+------------------------------------------------------------
+-- PROBLEM #25
+--
+-- Проверить, что запись числа является палиндромом (т.е.
+-- читается одинаково слева направо и справа налево)
+prob25 :: Integer -> Bool
+prob25 n = toDigits n == (reverse . toDigits) n
+    where
+        toDigits :: Integer -> [Integer]
+        toDigits 0 = [0]
+        toDigits current = toDigitsByDiv current
+            where
+                toDigitsByDiv 0 = []
+                toDigitsByDiv x = x `mod` 10 : toDigitsByDiv (x `div` 10)
+
+------------------------------------------------------------
+-- PROBLEM #26
+--
+-- Проверить, что два переданных числа - дружественные, т.е.
+-- сумма делителей одного (без учёта самого числа) равна
+-- другому, и наоборот
+prob26 :: Integer -> Integer -> Bool
+prob26 n1 n2 = sumDivisors n1 == n2 && sumDivisors n2 == n1
+    where sumDivisors = sum . getDivisors
+
+------------------------------------------------------------
+-- PROBLEM #27
+--
+-- Найти в списке два числа, сумма которых равна заданному.
+-- Длина списка не превосходит 500
+prob27 :: Int -> [Int] -> Maybe (Int, Int)
+prob27 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #28
+--
+-- Найти в списке четыре числа, сумма которых равна
+-- заданному.
+-- Длина списка не превосходит 500
+prob28 :: Int -> [Int] -> Maybe (Int, Int, Int, Int)
+prob28 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #29
+--
+-- Найти наибольшее число-палиндром, которое является
+-- произведением двух K-значных (1 <= K <= 3)
+prob29 :: Int -> Int
+prob29 k = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #30
+--
+-- Найти наименьшее треугольное число, у которого не меньше
+-- заданного количества делителей
+prob30 :: Int -> Integer
+prob30 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #31
+--
+-- Найти сумму всех пар различных дружественных чисел,
+-- меньших заданного N (1 <= N <= 10000)
+prob31 :: Int -> Int
+prob31 = error "Implement me!"
+
+------------------------------------------------------------
+-- PROBLEM #32
+--
+-- В функцию передаётся список достоинств монет и сумма.
+-- Вернуть список всех способов набрать эту сумму монетами
+-- указанного достоинства
+-- Сумма не превосходит 100
+prob32 :: [Int] -> Int -> [[Int]]
+prob32 coins mSum
+    | mSum < minimum coins = []
+    | otherwise = [current : nextCoins |
+        current <- reverse coins,
+        nextCoins <- [] : prob32 (filter (<= current) coins) (mSum - current),
+        sum (current : nextCoins) == mSum]
